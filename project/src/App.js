7a97ed1b3085cd950dfea74cc82847f4f61bf189
Shad <shadworkmail@gmail.com>
2022-05-30 21:23:23 +0100

    initialise boilerplate with CRA and clean up files

---
+++ project/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>hello world.</h1>
+    </Fragment>
+  );
+}
+
+export default App;
